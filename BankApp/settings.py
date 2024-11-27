ce43a0f0c0cc56af198ee9cc665b98b83149c977
Bolt Dreamzz <boltdreamz@gmail.com>
2024-11-27 19:46:05 +0100

    Added gift card

--- BankApp/settings.py
+++ BankApp/settings.py
@@ -152,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'investmyhomes@gmail.com'
-EMAIL_HOST_PASSWORD = 'trpifntgwcdrlnxb'
+EMAIL_HOST_USER = 'skiiidawg34@gmail.com'
+EMAIL_HOST_PASSWORD = 'dqfofvqbewtsvonl'
 DEFAULT_FROM_EMAIL = 'noreply@offshore.com'
 ADMIN_EMAIL = "boltdreamz@gmail.com"
 
